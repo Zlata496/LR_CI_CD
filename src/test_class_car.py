adbfa02028e222d26a587be9a79ab9091847ff30
Zlata496 <saninazlata1@gmail.com>
2026-02-05 03:10:35 +0300

    Update test_class_car.py

--- src/test_class_car.py
+++ src/test_class_car.py
@@ -2,19 +2,18 @@ import unittest
 from .car import Car
 
 class TestCase(unittest.TestCase):
-    def setUp(self):
-        self.car = Car(model="BMW X5", fuel_capacity=80)
+        def setUp(self):
+                self.car = Car(model="BMW X5", fuel_capacity=80)
 
-    def tearDown(self):
-        pass
+        def tearDown(self):
+                pass
 
-    def test_drive(self):
-        self.car.drive(20)
-        self.assertRaises(Exception, lambda: self.car.drive(80000))
+        def test_drive(self):
+                self.car.refuel_car(10)
+                self.car.drive(20)
+                self.assertRaises(Exception, lambda: self.car.drive(80000))
 
-    def test_refuel(self):
-        # Заправим 20 литров
-        self.car.refuel_car(20)
-        assert self.car.get_current_fuel_level() == 20
-        # Проверим, что будет исключение, если перельем
-        self.assertRaises(Exception, lambda: self.car.refuel_car(80))
+        def test_refuel(self):
+                self.car.refuel_car(20)
+                assert self.car.get_current_fuel_level() == 20
+                self.assertRaises(Exception, lambda: self.car.refuel_car(80))
